9d63a4fdf8cad60fd2d50e33ddf3eb15b2ee32ac
dillion241 <114444653+dillion241@users.noreply.github.com>
2022-10-29 08:59:46 +0100

    Add files via upload

---
+++ songcrud-master/musicapp/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from datetime import datetime
+
+
+# Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name
+
+class Song(models.Model):
+    Artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title = models.CharField(max_length=400)
+    date_released = models.DateField(default=datetime.today)
+    likes = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+class Lyrics(models.Model):
+    Song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.CharField(max_length=400)
+    song_id = models.IntegerField()
+
+    def __str__(self):
+        return self.content
